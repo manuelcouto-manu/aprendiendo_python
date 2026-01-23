617d24c0d0d8a185545ff7295e717a8b49c3fc5a
Manuel <manuel.couto@gmail.com>
2026-01-23 13:57:06 +0100

    función print

---
+++ modulo_2.py
@@ -0,0 +1,29 @@
+print("¡Bienvenidos a Python!")
+# Argumento: ¡Bienvenidos a Python! --> "Cadena de texto"
+# Función: print() --> Imprime en pantalla el argumento proporcionado
+# Descripción: Este código imprime un mensaje de bienvenida en la consola
+
+# nombre_funcion(argumento) --> invoca la función con el argumento dado
+
+# Una instrucción por linea es lo recomendado en Python
+
+print() #Imprime una línea en blanco
+
+print("¿Cómo estás?")
+
+print()
+
+print("Hoy es viernes \ny hace Sol") 
+# Carácter de escape y nueva línea:
+# \n es un carácter especial que indica un salto de línea
+
+print("lunes","martes","miércoles") 
+# Múltiples argumentos en print():
+# Se pueden pasar varios argumentos separados por comas
+# Genera la salida en una sola línea separados por espacios
+
+print("Hoy es viernes, qué bien")
+print("Fin de semana!")
+
+# manera posicional de pasar argumentos
+# print(arg1, arg2, arg3, ...)
